e2533a226edd24ca96dac7d3352f1366d022d6d0
Yeap Jia Sheng <66236001+jiasheng98@users.noreply.github.com>
2022-06-28 03:15:28 +0800

    sommit

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "3bbd692ca331d67190a2";
+/******/ 	var hotCurrentHash = "2470152be93895e49b92";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
