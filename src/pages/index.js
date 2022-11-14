cc6d40ef68ec69d6e9bb8e16ab9b67d9ae35e21f
Yeap Jia Sheng <66236001+jiasheng98@users.noreply.github.com>
2022-11-14 21:58:46 +0900

    index

--- src/pages/index.js
+++ src/pages/index.js
@@ -21,11 +21,12 @@ const Home = (props) => {
         <ContentRight />
         {/* <ContentLeft /> */}
       </div>
-      <TextContainer />
+      {/* <TextContainer /> */}
       <div class="container marketing">
-        <CardList />
-        <hr class="featurette-divider" />
+        {/* <CardList /> */}
+        {/* <hr class="featurette-divider" /> */}
         <PartnerCarousel />
+        <ClientCarousel />
       </div>
     </Layout>
   );
@@ -63,7 +64,7 @@ const LandingCarousel = () => {
           </div>
         </Carousel.Caption>
       </Carousel.Item>
-      {/* <Carousel.Item>
+      <Carousel.Item>
         <img
           className="d-block w-100"
           src="/images/IMG_1655 - Copy.jpg"
@@ -72,20 +73,19 @@ const LandingCarousel = () => {
         <Carousel.Caption>
           <div class="container">
             <div class="carousel-caption">
-              <h1 className="mb-3">TBJ Keeps You Running</h1>
+              <h1 className="mb-3">Aerolite</h1>
               <p className="mb-4">
-                Safety sealing and attachment solutions built for you. From
-                high-volume and automated to customized and one-of-a-kind.
+              Fire-safe certified, offering product range that provides sealing solution for durability and longevity.
               </p>
               <p>
-                <a class="btn btn-md" href="#" role="button">
-                  Contact Our Experts
+                <a class="btn btn-md" href="/product" role="button">
+                Discover our products
                 </a>
               </p>
             </div>
           </div>
         </Carousel.Caption>
-      </Carousel.Item> */}
+      </Carousel.Item>
     </Carousel>
   );
 };
@@ -103,11 +103,9 @@ const ImagewithText = () => {
             width="140"
             height="140"
           />
-          <h2 class="mb-3">Sealing Solutions</h2>
+          <h2 class="mb-3">Mission</h2>
           <p>
-            Meet Customers’ Requirement In Providing Customized, Specialized And
-            High-Quality Sealing Devices. Robustness, Accuracy And Precision Are
-            Our Main Features.
+          To deliver exceptional, individualized service and to be our customers’ supplier of choice.
           </p>
           {/* <p>
             <a class="btn btn-secondary" href="#" role="button">
@@ -123,11 +121,9 @@ const ImagewithText = () => {
             width="140"
             height="140"
           />
-          <h2 class="mb-3">Custom Cut Gasket</h2>
+          <h2 class="mb-3">Vision</h2>
           <p>
-            Fully Trained, High-Skilled Technicians And Engineers To Provide One
-            Stop Service For On-Site Repair, Installation, Troubleshooting And
-            Engineering Solutions.
+          To continue the heritage of our Company and be a premier Malaysian manufacturer of top quality sealing products. We strive to be a sealing gasket manufacturer to serve the local & global industries and provide total customer satisfaction. 
           </p>
           {/* <p>
             <a class="btn btn-secondary" href="#" role="button">
@@ -143,11 +139,9 @@ const ImagewithText = () => {
             width="140"
             height="140"
           />
-          <h2 class="mb-3">Corrosion and Metal Protection</h2>
+          <h2 class="mb-3">Values</h2>
           <p>
-            Advanced Material Formulations With Latest Process Technology,
-            Ensuring The Metal And Concrete Surfaces Are Free From Corrosion And
-            Chemical Attack.
+          We ensure to meet and listen to our customers’ requirements and provide the best and quality sealing products and services.  
           </p>
           {/* <p>
             <a class="btn btn-secondary" href="#" role="button">
@@ -172,7 +166,7 @@ const ContentRight = () => {
           Gasket, Ring Joint Gasket, Kamprofile Gasket, Metal Jacketed Gasket,
           Gland Packing, & other sealing products in the industry, we are
           continuously upgrading our products and services with the intention to
-          cater only our best to valued clients.
+          cater only our best to valued clients. <br/> <br/>We also developed our own house brand, Aerolite in 2012. Aerolite gaskets are fire-safe certified, which make them more durable and cost-effective. It has been widely used and accepted in the industries locally and regionally. 
         </p>
       </div>
       <div class="col">
@@ -237,6 +231,79 @@ const PartnerCarousel = () => {
   return (
     <>
       <h1 class="text-center text-highlight">Our Brands</h1>
+      <CarouselMulti responsive={responsive} infinite>
+        <div>
+          <a href="/" target="_blank" style={{ cursor: "pointer" }}>
+            <img
+              src={require("../../public/images/1.png")}
+              className="carousel-image"
+            />
+          </a>
+        </div>
+        <div>
+          <a href="/" target="_blank" style={{ cursor: "pointer" }}>
+            <img
+              src={require("../../public/images/3.png")}
+              className="carousel-image"
+            />
+          </a>
+        </div>
+        <div>
+          <a href="/" target="_blank" style={{ cursor: "pointer" }}>
+            <img
+              src={require("../../public/images/4.png")}
+              className="carousel-image"
+            />
+          </a>
+        </div>
+        <div>
+          <a href="/" target="_blank" style={{ cursor: "pointer" }}>
+            <img
+              src={require("../../public/images/5.png")}
+              className="carousel-image"
+            />
+          </a>
+        </div>
+        <div>
+          <a href="/" target="_blank" style={{ cursor: "pointer" }}>
+            <img
+              src={require("../../public/images/6.png")}
+              className="carousel-image"
+            />
+          </a>
+        </div>
+      </CarouselMulti>
+    </>
+  );
+};
+
+const ClientCarousel = () => {
+  const responsive = {
+    superLargeDesktop: {
+      // the naming can be any, depends on you.
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5,
+      infinite: true,
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 5,
+      infinite: true,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 4,
+      infinite: true,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 2,
+      infinite: true,
+    },
+  };
+  return (
+    <>
+      <h1 class="text-center text-highlight">Our Clients</h1>
       <CarouselMulti responsive={responsive} infinite>
         <div>
           <a href="/" target="_blank" style={{ cursor: "pointer" }}>
